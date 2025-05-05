980ec3b6cba4a0081d809b2928bc102fb406d432
Gabriela-Baroni <gabrielafb3@al.insper.edu.br>
2025-05-05 14:00:32 -0300

    commit 2 ex.12

--- funcoes.py
+++ funcoes.py
@@ -191,9 +191,9 @@ def calcula_pontos_regra_avancada(faces_a):
     }
     return dicionario
 
-def faz_jogada(dados,string,dic):
+def faz_jogada(dados,string,cartela_de_pontos):
     if string in ["1","2","3","4","5","6"]:
-        indice = int(categoria)
+        indice = int(string)
         if cartela_de_pontos["regras_simples"][indice] == -1:
             simples = calcula_pontos_regra_simples(dados)
             cartela_de_pontos["regras_simples"][indice] = simples[indice]
