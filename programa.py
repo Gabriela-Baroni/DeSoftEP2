02485fabbd9ccc3557d154d549bdfe19b6399a71
Gabriela-Baroni <gabrielafb3@al.insper.edu.br>
2025-05-06 21:11:11 +0000

    commit 2 ex.13

--- programa.py
+++ programa.py
@@ -1,6 +1,25 @@
 import funcoes
 from funcoes import *
 
+cartela_de_pontos = {
+    'regra_simples':  {
+        1:-1,
+        2:-1,
+        3:-1,
+        4:-1,
+        5:-1,
+        6:-1
+    },
+    'regra_avancada' : {
+        'sem_combinacao':-1,
+        'quadra': -1,
+        'full_house': -1,
+        'sequencia_baixa': -1,
+        'sequencia_alta': -1,
+        'cinco_iguais': -1
+    }
+}
+
 rodada = 0
 while rodada <= 12:
     rolados = rolar_dados(5)
